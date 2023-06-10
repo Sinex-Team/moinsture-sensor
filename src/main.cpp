cdb796a7a4edc09cd8d156ec084f3d8539bf45b7
igorv404 <igorvilkov404@gmail.com>
2023-06-10 11:52:02 +0300

    added communication with water system

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,12 @@ StaticJsonDocument<100> dataJSON;
 HTTPClient http;
 String sensor;
 
-const int moinstureSensor = 36;
+const int moinstureSensor = 34;
+const int relePin = 23;
 
 const char* ssid = "";
 const char* passowrd = "";
-const String serverUrl = "";
+const String serverUrl = "https://backend-aquamole.azurewebsites.net/hygrometers";
 
 int sensorId;
 int sensorMoinsture;
@@ -25,7 +26,7 @@ void setup() {
 void loop() {
   if (WiFi.isConnected()) {
     if (!sensorId) {
-      http.begin(serverUrl);
+      http.begin("https://backend-aquamole.azurewebsites.net/hygrometers/id");
       if (http.GET() == 200) {
         deserializeJson(dataJSON, http.getString());
         sensorId = dataJSON["id"];
@@ -35,6 +36,11 @@ void loop() {
     
     http.begin(serverUrl);
     sensorMoinsture = map(analogRead(moinstureSensor), 1000, 3000, 100, 0);
+    if (sensorMoinsture < 60) {
+      digitalWrite(relePin, 1);
+    } else if(sensorMoinsture > 80) {
+      digitalWrite(relePin, 0);
+    }
     dataJSON["id"] = sensorId;
     dataJSON["moinsture"] = sensorMoinsture;
     serializeJson(dataJSON, sensor);
